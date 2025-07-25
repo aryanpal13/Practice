d3caae5d0e7b86618e51e54a9c23dbc70146b63b
Aryan Pal <aryanpal401@gmail.com>
2025-07-25 20:00:03 +0530

    Added new files

---
+++ armstrongNo.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isArmstrongNo(int n) {
+    int dup = n;
+    int sum = 0;
+    int lastDigit;
+    while (n>0) {
+        lastDigit = n % 10;
+        n = n / 10;
+        sum = sum + (lastDigit * lastDigit * lastDigit);
+    }
+    if (sum == dup) return true;
+    else return false;
+}
+
+int main () {
+    int n;
+    cin >> n;
+    cout << isArmstrongNo(n) <<  endl;
+    return 0;
+}
