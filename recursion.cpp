8cd32949fd35ceaf7735259529fec23300bcba6d
Aryan Pal <aryanpal401@gmail.com>
2025-07-26 19:56:31 +0530

    Updated recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//function to print name n times using recursion
 void name(int i, int n) {
     if(i>n) {
         return;
@@ -11,6 +13,7 @@ void name(int i, int n) {
     
 }
 
+//function print number from 1 to n using recursion
 void nNo(int i, int n) {
     if(i>n) {
         return;
@@ -20,14 +23,33 @@ void nNo(int i, int n) {
     }
 }
 
+//function print reverse of an array using recursion
+void revArray (int arr[], int i, int n) {
+    if(i>=n/2) {
+        return;
+    } else{
+        swap(arr[i], arr[n-i-1]);
+        revArray(arr, i+1, n);
+    }
+}
+
 
 int main () {
-    int i = 1;
     int n;
-    cout << "Enter a number: ";
-    cin>>n;
+    // cout << "Enter a number: ";
+    // cin>>n;
     // name(1,n);
-    nNo(i, n);
+    // nNo(1, n);
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the values of the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    revArray(arr, 0, n);
+    for(int i=0;i<n;i++) cout<< arr[i] << " ";
+    cout<<endl;
 
     return 0;
 }
