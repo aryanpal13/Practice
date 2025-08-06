8078a6ebae0fae069a8827149136b4c908ed8258
Aryan Pal <aryanpal401@gmail.com>
2025-08-06 16:29:10 +0530

    Updated file

--- swapAlternate.cpp
+++ swapAlternate.cpp
@@ -1,33 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int swapAlternate(int arr[], int n, int i) {
-    if(i>= n) {
-        return 0;
-    } else {
-        if(i+1 < n) {
-            swap(arr[i], arr[i+1]);
-            return swapAlternate(arr, n, i+2);
+int swapAlternate(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i += 2)
+    {
+        if (i + 1 < n)
+        {
+            swap(arr[i], arr[i + 1]);
         }
-        
     }
 }
 
-int main() {
+int main()
+{
     int n;
-    cout<<"Enter the size of array: ";
-    cin>> n;
+    cout << "Enter the size of array: ";
+    cin >> n;
     int arr[n];
-    cout<<"Enter the values of the array: ";
-    for(int i=0;i<n;i++) {
-        cin>> arr[i];
+    cout << "Enter the values of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    swapAlternate(arr, n, 0);
+    swapAlternate(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-    
-
-
 }
